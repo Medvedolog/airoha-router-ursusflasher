2537e65afdabbce56556bd7e1c1ab23a728e146f
medvedolog <tenkayyy8@gmail.com>
2026-09-14 23:17:23 +0300

    ursusboot: make MD profile marker compile-time literal

--- ursusboot/board-policies/xg040-md.h
+++ ursusboot/board-policies/xg040-md.h
@@ -3,6 +3,7 @@
 #define __URSUS_BOARD_POLICY_H__
 
 #define URSUS_BOARD_POLICY_ID              "xg040-md"
+#define URSUS_BOARD_PROFILE_MARKER         "URSUS_BOARD_PROFILE=xg040-md"
 #define URSUS_BOARD_MODEL                  "Nokia XG-040G-MD"
 #define URSUS_BOARD_COMPATIBLE             "nokia,xg-040g-md"
 
